3ae326649be2a4a2d2dc5e440517b5c934de69ee
anirvan90 <anirvan.awatramani@gmail.com>
2015-12-17 17:24:27 +0530

    Create DB schema and object architecture

---
+++ database_setup.py
@@ -0,0 +1,47 @@
+import os
+import sys
+from sqlalchemy import Column, Integer, Foreign Key, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm	import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+#Creates table to store Item Categories
+class Category(Base):
+	__tablename__='category'
+
+	name = Column(String(80), nullable = False)
+	id = Column(Integer, primary_key = True)
+
+#Define property to return JSON objects 	
+	@property
+	def serialize(self):
+		return {
+			'name': self.name,
+			'id': self.id
+		}
+
+
+
+class Item(Base):
+	__tablename__='item'
+
+	name = Column(String(80), nullable = False)
+	id = Column(Integer, primary_key = True)
+	description = Column(String(250))
+	category_id = Column(Integer, ForeignKey('category.id'))
+	category = relationship(Category)
+
+	@property
+	def serialize(self):
+
+		return {
+			'name': self.name,
+			'description': self.description,
+			'id': self.id,
+			'category': self.category
+		}
+
+engine = create_engine('sqlite:///itemcatalog.db')
+Base.metadata.create_all(engine)
