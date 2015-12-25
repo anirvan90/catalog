884d0e0433df7e68f6124e68f263783c486ceb3f
anirvan90 <anirvan.awatramani@gmail.com>
2015-12-25 18:12:48 +0530

    Add Image Server. Fail.

--- database_setup.py
+++ database_setup.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, Integer, ForeignKey, String, LargeBinary
+from sqlalchemy import Column, Integer, ForeignKey, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm	import relationship
 from sqlalchemy import create_engine
