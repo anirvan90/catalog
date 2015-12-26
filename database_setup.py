ba942905e8686f46052aa47ff75e47c83d7c3956
anirvan90 <anirvan.awatramani@gmail.com>
2015-12-26 10:03:20 +0530

    Check with venv

--- database_setup.py
+++ database_setup.py
@@ -2,6 +2,7 @@ import os
 import sys
 from sqlalchemy import Column, Integer, ForeignKey, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_imageattach.entity import Image, image_attachment
 from sqlalchemy.orm	import relationship
 from sqlalchemy import create_engine
 
